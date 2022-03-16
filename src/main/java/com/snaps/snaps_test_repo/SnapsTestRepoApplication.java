30ccf940b852a4ae103bce59b9e675a1874a8ed9
joshua7235 <joshua@snaps.com>
2022-03-16 13:29:21 +0900

    First git init

---
+++ src/main/java/com/snaps/snaps_test_repo/SnapsTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.snaps.snaps_test_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapsTestRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapsTestRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snaps/snaps_test_repo/SnapsTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snaps.snaps_test_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapsTestRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
